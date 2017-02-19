095f9b0485bf9d709e7ade13483ac31e75346c61
DigitalWhale <jargiv@ya.ru>
2017-02-20 01:50:02 +0300

    Deleted css filter, created and used the custom filters

--- webcam.js
+++ webcam.js
@@ -2,48 +2,84 @@
  * Created by Ярослав on 16.02.2017.
  */
 
+
+let sepiaFilter = (imageData, range) =>{
+    let pixelChannel = imageData.data;
+    for(let i = 0; i < pixelChannel.length; i += 4){
+        let r = pixelChannel[i];
+        let g = pixelChannel[i + 1];
+        let b = pixelChannel[i + 2];
+        pixelChannel[i] =  r*(100-range)/100 + ((r + g + b)/3 + 2*30)*(range/100);
+        pixelChannel[i + 1] =  g*(100-range)/100 + ((r + g + b)/3 + 30)*(range/100);
+        pixelChannel[i + 2] =  b*(100-range)/100 + ((r + g + b)/3)*(range/100);
+    }
+    return imageData;
+};
+
+let grayscaleFilter = (imageData, range) =>{
+    let pixelChannel = imageData.data;
+    for(let i = 0; i < pixelChannel.length; i += 4){
+        let r = pixelChannel[i];
+        let g = pixelChannel[i + 1];
+        let b = pixelChannel[i + 2];
+        pixelChannel[i] =  r*(100-range)/100 + ((r + g + b)/3)*(range/100);
+        pixelChannel[i + 1] =  g*(100-range)/100 + ((r + g + b)/3)*(range/100);
+        pixelChannel[i + 2] =  b*(100-range)/100 + ((r + g + b)/3)*(range/100);
+    }
+    return imageData;
+};
+
+let invertFilter = (imageData, range) =>{
+    let pixelChannel = imageData.data;
+    for(let i = 0; i < pixelChannel.length; i += 4){
+        pixelChannel[i] =  (255 - pixelChannel[i])*range/100 + pixelChannel[i]*(100-range)/100;
+        pixelChannel[i + 1] =  (255 - pixelChannel[i + 1])*range/100 + pixelChannel[i+1]*(100-range)/100;
+        pixelChannel[i + 2] =  (255 - pixelChannel[i + 2])*range/100 + pixelChannel[i+2]*(100-range)/100;
+    }
+    return imageData;
+};
+
+
+
     //Набор фильтров и их параметров
 let filters = {
-        blur: {
-            name: "blur",
-            value: 50,
-            enable: false
-        },
         grayscale: {
             name: "grayscale",
             value: 100,
-            enable: false
+            enable: false,
+            func: grayscaleFilter
         },
         sepia: {
             name: "sepia",
             value: 100,
-            enable: false
+            enable: false,
+            func: sepiaFilter
         },
         invert: {
             name: "invert",
             value: 100,
-            enable: false
+            enable: false,
+            func: invertFilter
         }
     };
 
 window.onload = ()=>{
     let video = document.getElementById("vid"),
         canvas = {
+            canvVideo: document.getElementById("canvVideo"),
             canvMain: document.getElementById("canv"),
-            canvBlur: document.getElementById("canvBlur"),
             canvSepia: document.getElementById("canvSepia"),
             canvGray: document.getElementById("canvGray"),
             canvInvert: document.getElementById("canvInvert")
         },
         ctx = {
+            ctxVideo: canvas.canvVideo.getContext("2d"),
             ctxMain: canvas.canvMain.getContext("2d"),
-            ctxBlur: canvas.canvBlur.getContext("2d"),
             ctxSepia: canvas.canvSepia.getContext("2d"),
             ctxGray: canvas.canvGray.getContext("2d"),
             ctxInvert: canvas.canvInvert.getContext("2d")
         },
         range = {
-            blur: document.getElementById("rangeB"),
             sepia: document.getElementById("rangeS"),
             grayscale: document.getElementById("rangeG"),
             invert: document.getElementById("rangeI"),
@@ -51,15 +87,6 @@ window.onload = ()=>{
         btn = document.getElementById("btn"),
         localMediaStream = null;
 
-    //Сборка валидной строки из объекта filter
-    let stringFilter = (filter, value) => {
-        if(filter !== "blur"){
-            return filter + "(" + value + "%)";
-        }
-        else {
-            return filter + "(" + value/10 + "px)" ;
-        }
-    };
 
     //Перехват видео потока с камеры на тэг video
     let initVideoStream = (stream) => {
@@ -82,48 +109,24 @@ window.onload = ()=>{
     //Отрисока кадра видео на канвасе
     let videoToCanvas = (context, filter)=>{
         if(localMediaStream){
-            if(filter) {
-                context.filter = stringFilter(filter.name, filter.value);
-            }
-            else{
-                if(video.style.filter.toString() == ""){
-                    context.filter = "none";
-                }else {
-                    context.filter = video.style.filter;
-                }
-               console.log("v"+video.style.filter.toString()+"v");
-               console.log(context.filter.toString());
-            }
             context.drawImage(video, 0, 0);
+            setFilter(context, filter);
         }
     };
 
-    //Наложение или удаление фильтров на видео
-    let videoFilter = (filter) => {
-        let strFilter = stringFilter(filter.name, filter.value);
-
-        //Скрыть "бегунки" и подписи настроек фильтров
-        let styleRange = document.querySelectorAll("input[type=range]");
-        let styleLabelRange = document.querySelectorAll("label");
-        for(let i = 0; i < styleRange.length; i++){
-          styleRange[i].style.display = "none";
-          styleLabelRange[i].style.display = "none"
-        }
-
-        //Если фильтр не наложен, то включаем его, отображаем его настройки и накладываем на изображение
-        //Иначе выключаем фильтр и снимаем его с изображения
-        if(!filter.enable) {
-            video.style.filter +=  strFilter;
-            document.querySelector("label[name = "+filter.name+"]").style.display = "block";
-            range[filter.name].style.display = "block";
+    let setFilter = (context, filter) => {
+        let dataImg = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+        if(filter){
+            dataImg = filter.func(dataImg, 100);
         }
         else{
-            let videoStyle = video.style.filter.toString();
-            videoStyle = videoStyle.replace(strFilter, "");
-            console.log(videoStyle);
-            video.style.filter = videoStyle;
+            for(let value in filters){
+                if(filters[value].enable){
+                    dataImg = filters[value].func(dataImg, filters[value].value);
+                }
+            }
         }
-        filter.enable = !filter.enable;
+        context.putImageData(dataImg, 0, 0);
     };
 
 
@@ -132,36 +135,72 @@ window.onload = ()=>{
 
     //Отображаем превью фильтров
     cameraInterval = setInterval(() => {
-        videoToCanvas(ctx.ctxBlur, filters.blur);
+        videoToCanvas(ctx.ctxVideo, null);
         videoToCanvas(ctx.ctxGray, filters.grayscale);
         videoToCanvas(ctx.ctxSepia, filters.sepia);
         videoToCanvas(ctx.ctxInvert, filters.invert);
-    }, 1 );
+    }, 40 );
 
     //Изменяем плотность фильтра при помощи бегунка
     let setValue = (filterName) => {
-        let strFilter = stringFilter(filterName, filters[filterName].value);
-        let videoStyle = video.style.filter.toString();
-        videoStyle = videoStyle.replace(strFilter, "");
-        console.log(strFilter);
         filters[filterName].value = range[filterName].value;
-        strFilter = stringFilter(filterName, filters[filterName].value);
-        console.log(strFilter);
-        video.style.filter = videoStyle + " " + strFilter ;
-
     };
 
     //Кнопка сохранения кадра
     btn.addEventListener("click", () => {videoToCanvas(ctx.ctxMain, null)});
 
+    let viewSetting = () => {
+        let countEnableFilter = 0;
+        if(filters.grayscale.enable){
+              countEnableFilter++;
+              range.grayscale.style.display = "block";
+              range.grayscale.style.marginTop = 60 - (5*countEnableFilter) + "%";
+              document.getElementsByName("grayscale")[0].style.display = "block";
+              document.getElementsByName("grayscale")[0].style.marginTop = 57 - (5*countEnableFilter) + "%";
+        }
+        else {
+            range.grayscale.style.display = "none";
+            document.getElementsByName("grayscale")[0].style.display = "none";
+        }
+        if(filters.sepia.enable){
+            countEnableFilter++;
+            range.sepia.style.display = "block";
+            range.sepia.style.marginTop = 60 - (5*countEnableFilter) + "%";
+            document.getElementsByName("sepia")[0].style.display = "block";
+            document.getElementsByName("sepia")[0].style.marginTop = 57 - (5*countEnableFilter) + "%";
+        }
+        else {
+            range.sepia.style.display = "none";
+            document.getElementsByName("sepia")[0].style.display = "none";
+        }
+        if(filters.invert.enable){
+            countEnableFilter++;
+            range.invert.style.display = "block";
+            range.invert.style.marginTop = 60 - (5*countEnableFilter) + "%";
+            document.getElementsByName("invert")[0].style.display = "block";
+            document.getElementsByName("invert")[0].style.marginTop = 57 - (5*countEnableFilter) + "%";
+        }
+        else {
+            range.invert.style.display = "none";
+            document.getElementsByName("invert")[0].style.display = "none";
+        }
+    };
+
     //Кнопки наложения фильтров на видео
-    canvas.canvBlur.addEventListener("click", () => {videoFilter(filters.blur)});
-    canvas.canvGray.addEventListener("click", () => {videoFilter(filters.grayscale)});
-    canvas.canvSepia.addEventListener("click", () => {videoFilter(filters.sepia)});
-    canvas.canvInvert.addEventListener("click", () => {videoFilter(filters.invert)});
+    canvas.canvGray.addEventListener("click", ()=>{
+        filters.grayscale.enable = !filters.grayscale.enable;
+        viewSetting();
+    });
+    canvas.canvSepia.addEventListener("click", ()=>{
+        filters.sepia.enable = !filters.sepia.enable;
+        viewSetting();
+    });
+    canvas.canvInvert.addEventListener("click", ()=>{
+        filters.invert.enable = !filters.invert.enable;
+        viewSetting();
+    });
 
     //Значение бегунка
-    range.blur.addEventListener("input", () => {setValue("blur");});
     range.grayscale.addEventListener("input", () => {setValue("grayscale");});
     range.sepia.addEventListener("input", () => {setValue("sepia");});
     range.invert.addEventListener("input", () => {setValue("invert");});
